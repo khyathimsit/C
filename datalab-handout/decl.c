fd9c3c465694af620f2dd4ce77ced18db062bb86
khyathimsit <41896483+khyathimsit@users.noreply.github.com>
2020-07-13 22:14:20 +0530

    Add files via upload

---
+++ datalab-handout/decl.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define TMin INT_MIN
+#define TMax INT_MAX
+
+#include "btest.h"
+#include "bits.h"
+
+test_rec test_set[] = {
+/* Copyright (C) 1991-2016 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+/* This header is separate from features.h so that the compiler can
+   include it implicitly at the start of every compilation.  It must
+   not itself include <features.h> or any other header that includes
+   <features.h> because the implicit include comes before any feature
+   test macros that may be defined in a source file before it first
+   explicitly includes a system header.  GCC knows the name of this
+   header in order to preinclude it.  */
+/* glibc's intent is to support the IEC 559 math functionality, real
+   and complex.  If the GCC (4.9 and later) predefined macros
+   specifying compiler intent are available, use them to determine
+   whether the overall intent is to support these features; otherwise,
+   presume an older compiler has intent to support these features and
+   define these macros by default.  */
+/* wchar_t uses Unicode 8.0.0.  Version 8.0 of the Unicode Standard is
+   synchronized with ISO/IEC 10646:2014, plus Amendment 1 (published
+   2015-05-15).  */
+/* We do not support C11 <threads.h>.  */
+ {"implication", (funct_t) implication, (funct_t) test_implication, 2, "! ~ ^ |", 5, 2,
+     {{0,1},{0,1},{TMin,TMax}}},
+ {"isTmin", (funct_t) isTmin, (funct_t) test_isTmin, 1, "! ~ & ^ | +", 10, 1,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"anyOddBit", (funct_t) anyOddBit, (funct_t) test_anyOddBit, 1,
+    "! ~ & ^ | + << >>", 12, 2,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"satAdd", (funct_t) satAdd, (funct_t) test_satAdd, 2,
+    "! ~ & ^ | + << >>", 30, 4,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"divpwr2", (funct_t) divpwr2, (funct_t) test_divpwr2, 2,
+    "! ~ & ^ | + << >>", 15, 2,
+  {{TMin, TMax},{0,30},{TMin,TMax}}},
+ {"getByte", (funct_t) getByte, (funct_t) test_getByte, 2,
+    "! ~ & ^ | + << >>", 6, 2,
+  {{TMin, TMax},{0,3},{TMin,TMax}}},
+ {"float_abs", (funct_t) float_abs, (funct_t) test_float_abs, 1,
+    "$", 10, 2,
+     {{1, 1},{1,1},{1,1}}},
+ {"leftBitCount", (funct_t) leftBitCount, (funct_t) test_leftBitCount, 1, "! ~ & ^ | + << >>", 40, 4,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+  {"", NULL, NULL, 0, "", 0, 0,
+   {{0, 0},{0,0},{0,0}}}
+};
